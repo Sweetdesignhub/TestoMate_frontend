de896d2ca5f671f117c88d36f62b72eb894c93b2
Sweetdesignhub <sweetdesignhub@gmail.com>
2025-07-22 12:24:05 +0530

    Update RequirementsPage.jsx

--- src/pages/RequirementsPage.jsx
+++ src/pages/RequirementsPage.jsx
@@ -526,7 +526,7 @@ export default function RequirementsPage() {
     // First validate the prompt
     try {
       const validationResponse = await axios.post(
-        "http://20.244.44.70:5001/generate_contents",
+        "https://testomate-python-backend-bgemdxgpgwdccfhb.southindia-01.azurewebsites.net/generate_contents",
         { requirement }
       );
 
